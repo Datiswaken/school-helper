d0b6e02692e5028fcdcf333a518ec6626b23a6d6
datiswaken <jannis.lambinus@web.de>
2023-12-11 14:57:25 +0100

    Make actions repeatable

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import argparse
 from typing import List
 
 from src.school_class import SchoolClass
@@ -21,6 +20,7 @@ GROUP_NAMES = ("You can specify group names. If you skip this step groups will b
                "separated by commas. To skip just press ENTER.\n")
 NUMBER_OF_PUPILS_PER_GROUP = "How many pupils should each group consist of? "
 MAKE_GROUPS_OF_N_ACTION = "3"
+NEW_ACTION = "Press enter to choose a new action or press 'q' to quit. "
 
 
 def mark_sick_pupils(school_class: SchoolClass, pupil_names: List[str]) -> None:
@@ -42,9 +42,8 @@ def run():
         input_request=SICK_PUPILS_QUESTION,
         options=[YES, NO],
         skippable=True,
-        default="no"
+        default=NO
     ).request_user_input()
-    print(sick_pupils_question)
     if sick_pupils_question is not NO:
         sick_pupils = UserInput(
             input_request=SICK_PUPILS
@@ -63,27 +62,33 @@ def run():
             if consider_sick_pupils == YES:
                 only_healthy = False
 
-    action = UserInput(
-        input_request=ACTION_INPUT,
-        options=["1", "2", "3"],
-    ).request_user_input()
-    if action == DRAW_RND_ACTION:
-        pupil = school_class.draw_pupil(only_healthy=only_healthy)
-        print(f"Randomly drawn pupil is {pupil.name}")
-    elif action == SPLIT_INTO_GROUPS_ACTION:
-        number_of_groups = int(input(NUMBER_OF_GROUPS))
-        group_names = input(GROUP_NAMES)
-        group_names = group_names if group_names != "" else None
-        for group in school_class.split_into_n_groups(
-                n=number_of_groups,
-                group_names=group_names,
-                only_healthy=only_healthy
-        ):
-            print(group)
-    elif action == MAKE_GROUPS_OF_N_ACTION:
-        number_of_pupils_per_group = int(input(NUMBER_OF_PUPILS_PER_GROUP))
-        for group in school_class.make_groups_of_n_pupils(n=number_of_pupils_per_group, only_healthy=only_healthy):
-            print(group)
+    exit_script = False
+    while not exit_script:
+        action = UserInput(
+            input_request=ACTION_INPUT,
+            options=["1", "2", "3"],
+        ).request_user_input()
+        if action == DRAW_RND_ACTION:
+            pupil = school_class.draw_pupil(only_healthy=only_healthy)
+            print(f"Randomly drawn pupil is {pupil.name}")
+        elif action == SPLIT_INTO_GROUPS_ACTION:
+            number_of_groups = int(input(NUMBER_OF_GROUPS))
+            group_names = input(GROUP_NAMES)
+            group_names = group_names if group_names != "" else None
+            for group in school_class.split_into_n_groups(
+                    n=number_of_groups,
+                    group_names=group_names,
+                    only_healthy=only_healthy
+            ):
+                print(group)
+        elif action == MAKE_GROUPS_OF_N_ACTION:
+            number_of_pupils_per_group = int(input(NUMBER_OF_PUPILS_PER_GROUP))
+            for group in school_class.make_groups_of_n_pupils(n=number_of_pupils_per_group, only_healthy=only_healthy):
+                print(group)
+
+        new_action = UserInput(NEW_ACTION, options=["ENTER", "q"], skippable=True, default="new_action").request_user_input()
+        if new_action == "q":
+            exit_script = True
 
 
 if __name__ == '__main__':
